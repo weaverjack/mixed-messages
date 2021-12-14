e96867322a1ffc9e422cc9ffd55882841a7fbb14
weaverjack <ghia63@gmail.com>
2021-12-13 19:16:41 -0700

    created object with random message content

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mixed-messages
+## Project Objectives
+
++ create random message generator
++ version control with git
++ use commandline
---
+++ random-message-generator-project.js
@@ -0,0 +1,9 @@
+
+
+//create object to hold the message elements that will be randomized. 
+const thingsToDo = {
+    places: ['to Moab', 'to Hawaii', 'nowhere, just stay home', 'anywhere, pick a random spot on the map'],
+    activities: ['ride a bike on a fun trail', 'go out to eat as much as you can tolerate', 'relax and have a cocktail or 10'],
+    mindset: ['try to relax as much as possible', 'go crazy put your all into what your doing', 'really get to know the area']
+};
+
